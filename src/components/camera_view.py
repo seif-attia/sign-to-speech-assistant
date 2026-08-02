ba077607d00e1b481a3c02011803efc56ded749f
seif-attia <seifislam871@gmail.com>
2026-08-02 18:33:02 +0300

    Basic camera view implementation

---
+++ src/components/camera_view.py
@@ -0,0 +1,94 @@
+import flet as ft
+import flet_permission_handler as fh
+import flet_camera as fc
+
+
+class CameraView(ft.Column):
+    def __init__(
+            self, 
+            width: int,
+            height: int, 
+            resolution: fc.ResolutionPreset = fc.ResolutionPreset.MEDIUM, 
+            lens_direction: fc.CameraLensDirection = fc.CameraLensDirection.FRONT
+    ) -> None:
+        super().__init__()
+
+        # Camera Properties
+        self.camera_width :int = width
+        self.camera_height: int = height
+        self.camera_resolution: fc.ResolutionPreset = resolution
+        self.camera_lens_direction: fc.CameraLensDirection = lens_direction
+
+
+        # Camera UI Elements
+        self.camera: fc.Camera = fc.Camera(preview_enabled=True, expand=True)
+        self.permission_handler = fh.PermissionHandler()
+
+        self.camera_container = ft.Container(
+            content=self.camera,
+            width=self.camera_width,
+            height=self.camera_height,
+            bgcolor= ft.Colors.BLACK,
+            border_radius=8,
+        )
+
+
+        self.status_text = ft.Text(value="Initializing camera....", color=ft.Colors.GREY_400)
+
+
+        # Mount UI Elements to page before calling it
+        self.controls=[self.camera_container, self.status_text]
+        self.horizontal_alignment= ft.CrossAxisAlignment.CENTER
+        self.alignment= ft.MainAxisAlignment.START
+
+    def did_mount(self) -> None:
+        self.page.run_task(self._setup_camera)
+        
+
+    async def _setup_camera(self) -> None:
+
+        has_permission: fh.PermissionStatus | None = self.permission_handler.request(fh.Permission.CAMERA)
+
+        if not has_permission:
+            self.status_text.value = "Camera permission denied"
+            self.status_text.color = ft.Colors.RED
+            self.update()
+            return
+
+        self.status_text.value = "Checking available cameras..."
+        self.update()
+
+        cameras: list[fc.CameraDescription] = await self.camera.get_available_cameras()
+        if not cameras:
+            self.status_text.value = "No camera detected on this device"
+            self.status_text.color = ft.Colors.RED
+            self.update()
+            return
+
+        # Get desired camera target based on selected direction
+        target_indx = 0
+        for idx, cam in enumerate(cameras):
+            if cam.lens_direction == self.camera_lens_direction:
+                target_indx: int = idx
+                break
+
+        try:
+            self.status_text.value = "Initiliazing camera..."
+            self.update()
+
+            await self.camera.initialize(
+                description=cameras[target_indx], 
+                resolution_preset=self.camera_resolution
+            )
+
+            self.status_text.value = "Camera ready."
+            self.status_text.color = ft.Colors.GREEN_500
+            self.update()
+        except Exception as err:
+            self.status_text.value = f"Initilization Error: {err}"
+            self.status_text.color = ft.Colors.RED
+            self.update()
+        
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
 import flet as ft
+import flet_camera as fc
+import flet_permission_handler as fh
+from components.camera_view import CameraView
 
+def main(page: ft.Page) -> None:
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.title = "Flet Camera" 
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    def increment_click(e: ft.Event[ft.FloatingActionButton]):
-        counter.data += 1
-        counter.value = str(counter.data)
+    camera_feed: CameraView = CameraView(width=320, height=420, lens_direction=fc.CameraLensDirection.BACK)
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, key="increment", on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
-    )
+    page.add(ft.Column(
+        alignment=ft.MainAxisAlignment.START,
+        controls= camera_feed
+    ))
 
-
+   
 if __name__ == "__main__":
     ft.run(main)
